8da8f1a40547ccf25587873007b8b5c3b97def8d
Steven Moniaga <164908075+stevenmoniaga14@users.noreply.github.com>
2024-07-22 21:03:51 -0400

    finished Task 2

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -29,31 +29,133 @@ function oldScrabbleScorer(word) {
 	return letterPoints;
  }
 
+ 
+
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
+
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let word = input.question("Let's play some scrabble! Enter a word: ");
+   return word;
 };
 
-let newPointStructure;
+const simpleScorer = (word) => {
+   word = word.toUpperCase();
+   let score = 0;
 
-let simpleScorer;
+   for (let i=0; i<word.length; i++)
+      score += 1;
 
-let vowelBonusScorer;
+   return score;
+}
 
-let scrabbleScorer;
+// let wordTest = "pineapple"
+// console.log(`simple scorer for ${wordTest}: ${simpleScorer(wordTest)}`)
 
-const scoringAlgorithms = [];
+const vowelBonusScorer = (word) => {
+   word = word.toUpperCase();
+   let vowels = "AEIOU"
+   let score = 0;
 
-function scorerPrompt() {}
+   for (let i = 0; i<word.length; i++) {
+      // Set a condition that for each iteration in word, when the iteration matches "AEIOU", it scores a 3. All other letters just get 1.
+      if (vowels.includes(word[i])) {
+       score += 3;
+   } else {
+      score += 1;
+   }
 
-function transform() {};
+}
+return score;
+}
+
+// let wordTest = "pineapple"
+// console.log(`vowel scorer for the word, ${wordTest}: ${vowelBonusScorer(wordTest)}`);
+
+const transform = (oldPointStructure) => {
+   
+   let freshStructure = {};
+  
+   for(let newPoint in oldPointStructure) {
+     // Gets the value from each array of oldPointStructure and storing them in letters
+     let letters = oldPointStructure[newPoint];   
+ 
+     // Inner loop loops through the array of values and matches with the letter
+     for (let i=0; i<letters.length; i++) {
+       let toLowerCaseLetter = letters[i].toLowerCase();
+       // this sets the value for the object freshStructure so output is for example - a:1, e:1, ...
+       freshStructure[toLowerCaseLetter] = Number(newPoint);
+     }
+ 
+   }
+   return freshStructure;
+ };
+
+
+
+const newPointStructure = transform(oldPointStructure)
+
+function scrabbleScorer(word) {
+   word = word.toLowerCase();
+   let score = 0;
+   // iterate each letter in the word and plug the value to the newPointStructure to get score
+   for (let i=0; i<word.length; i++) {
+      // accesses property in the object which correspond to the letter and its point
+      score += newPointStructure[word[i]];
+   }
+   return score;
+}
+
+const scoringAlgorithms = 
+   [ { name: "Simple Scorer",
+    description: "Each letter is worth 1 point.",
+    scorerFunction: simpleScorer
+},
+   {
+   name: "Bonus Vowels",
+   description: "Vowels are 3 pts, consonants are 1 pt",
+   scorerFunction: vowelBonusScorer
+
+},
+   {
+   name: "New Scrabble",
+   description: "New point structure scrabble scorer",
+   scorerFunction: scrabbleScorer
+}
+   ];
+
+
+
+ function scorerPrompt() {
+   console.log("Which scoring algorithm would you like to use?\n");
+   console.log("0 - Simple: One point per character");
+   console.log("1 - Vowel Bonus: Vowels are worth 3 points");
+   console.log("2 - Scrabble: Uses scrabble point system");
+
+   let theirChoice = input.question("Enter 0, 1, or 2: ")
+   theirChoice = Number(theirChoice);
+
+   if (theirChoice === 0) {
+      // Grabs the key value pair function from object scoringAlgorithms
+      return scoringAlgorithms[0].scorerFunction;
+   } else if (theirChoice === 1) {
+      return scoringAlgorithms[1].scorerFunction;
+   } else if (theirChoice === 2) {
+      return scoringAlgorithms[2].scorerFunction;
+   }
+
+};
 
 function runProgram() {
-   initialPrompt();
+   let word = initialPrompt();
+   let selectedScorer = scorerPrompt();
+   let score = selectedScorer(word);
+   console.log(`Score for ${word}: ${score}`);
    
-}
+};
+
+// runProgram();
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
